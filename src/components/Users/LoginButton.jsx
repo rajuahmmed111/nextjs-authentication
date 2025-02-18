dea127039bfcaa1e8587d7aded71ee66bdc16b1b
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-18 12:18:09 +0600

    update route and login button

--- src/components/Users/LoginButton.jsx
+++ src/components/Users/LoginButton.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { signIn } from "next-auth/react";
 
@@ -5,7 +6,7 @@ const LoginButton = () => {
   return (
     <div>
       <button
-        onClick={() => singIn()}
+        onClick={() => signIn()}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       >
         Login
