6a61668661bda7f9ea6b4845828d825444e874bf
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-18 12:13:20 +0600

    login button use home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import LoginButton from "@/components/Users/LoginButton";
 import React from "react";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
       <h1 className="text-4xl text-center">
         Welcome to next level data fetching module
       </h1>
+      <LoginButton />
     </div>
   );
 };
